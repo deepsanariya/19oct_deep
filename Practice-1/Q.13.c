4eef1d7e9bf373f45bd4111dbcb05b54ae6cdb4a
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-01-21 21:33:10 +0530

    Add files via upload

---
+++ Practice-1/Q.13.c
@@ -0,0 +1,8 @@
+//Find charecter value from ascii
+#include <stdio.h>
+void main(){
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+    printf("Ascii value of %c = %d", c, c);
+}
