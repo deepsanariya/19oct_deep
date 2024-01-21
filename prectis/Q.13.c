b5d25136b9568c5252c614c08549ceeb49f410d8
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-01-21 21:26:16 +0530

    Delete prectis directory

--- prectis/Q.13.c
+++
@@ -1,8 +0,0 @@
-//Find charecter value from ascii
-#include <stdio.h>
-void main(){
-    char c;
-    printf("Enter a character: ");
-    scanf("%c",&c);
-    printf("Ascii value of %c = %d", c, c);
-}
