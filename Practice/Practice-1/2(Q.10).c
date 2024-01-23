a6314e33394ec136db8ed23fd291b48ab655202f
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-01-23 14:54:39 +0530

    Delete Practice/Practice-1 directory

--- Practice/Practice-1/2(Q.10).c
+++
@@ -1,7 +0,0 @@
-
-#include<stdio.h>{
-    int A=2(wl+hl+hw)
-    A=2(wl+hl+hw)
-    A=(wl*2+hl*2+hw*2)
-    A=(2wl+2hl+2hw)
-}
--- Practice/Practice-1/Q.19.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    int salary;
-    printf("enter monthli salary");
-    scanf("%d",&salary);
-    salary=salary*(1+0.05/4)*4*3;
-    printf("compound interest %d",salary);
-}
