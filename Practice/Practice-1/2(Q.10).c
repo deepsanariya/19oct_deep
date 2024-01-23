9aef768c8098ef084581b4c7ae5cb90f4bbb75b2
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-01-23 15:08:20 +0530

    Add files via upload

---
+++ Practice/Practice-1/2(Q.10).c
@@ -0,0 +1,7 @@
+
+#include<stdio.h>{
+    int A=2(wl+hl+hw)
+    A=2(wl+hl+hw)
+    A=(wl*2+hl*2+hw*2)
+    A=(2wl+2hl+2hw)
+}
---
+++ Practice/Practice-1/Q.15.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main() {
+  char cna,e[20];
+  printf("Enter the country names: ");
+  scanf("%s",&cname);
+  printf("abbreviated Name: ");
+  printf("%c%c", cname[0], cname[1]);
+}
