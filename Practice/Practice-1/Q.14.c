4ccfe64ba8106b522a0d012a264303cf6bd1bd9a
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-02-15 12:05:42 +0530

    Delete Practice/Practice-1 directory

--- Practice/Practice-1/Q.14.c
+++
@@ -1,8 +0,0 @@
-//Find ascii value of given number
-#include<stdio.h>
-void main(){
-    char c;
-    printf("Enter a number: ");
-    scanf("%c",&c);
-    printf("ASCII value of %c = %d", c, c);
-}
--- Practice/Practice-1/Q.15.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main() {
-  char cna,e[20];
-  printf("Enter the country names: ");
-  scanf("%s",&cname);
-  printf("abbreviated Name: ");
-  printf("%c%c", cname[0], cname[1]);
-}
