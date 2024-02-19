a2c8118c2762f8cb6f64a4b00fa53fd55c52a74c
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-02-19 10:40:57 +0530

    Delete Practice/Practice-2 directory

--- Practice/Practice-2/2(Q.11).c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    int a;
-    printf("enter number");
-    scanf("%d",&a);
-        ((a%2)==0)?(printf("even")):(printf("odd"));
-    return 0;
-}
