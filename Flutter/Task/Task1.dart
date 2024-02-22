8eafb2e006c267e5f5eb0bb9912955fc4cdf322e
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-02-22 13:21:54 +0530

    Add files via upload

---
+++ Flutter/Task/Task1.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  print("Enter your name:");
+  print("Enter your birthday:");
+  print("Enter your age:");
+  print("Enter your address:");
+
+}
---
+++ Flutter/Task/Task2.dart
@@ -0,0 +1,16 @@
+void main()
+{
+    var a = 6;
+    var b=  3;
+
+    var sum = a+b;
+    var sub = a-b;
+    var mul = a*b;
+    var div = a/b;
+
+    print(sum);
+    print(sub);
+    print(mul);
+    print(div);
+
+}
