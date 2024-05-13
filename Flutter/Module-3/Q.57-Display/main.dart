2370b3dddd3070007094671f2e6a3b020e8b182f
deepsanariya <148442740+deepsanariya@users.noreply.github.com>
2024-05-13 09:44:33 +0530

    Add files via upload

---
+++ Flutter/Module-3/Q.57-Display/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(home: MyApp(),debugShowCheckedModeBanner: false,));
+}
+
+class MyApp extends StatefulWidget
+{
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>
+{
+
+  Color _defaultColor = Colors.white;
+
+  void _onColorSeleted(Color color)
+  {
+    setState(()
+    {
+      _defaultColor = color;
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+      (
+      appBar: AppBar(title: Text("Change BG color using RadioButton")),
+
+      body:Container
+        (
+        color: _defaultColor,
+        child: Column
+          (
+          children:
+          [
+            SizedBox(height: 30,),
+            Text("Change BG Color",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+
+            SizedBox(height: 100,),
+
+            RadioListTile(
+                title: Text("Red"),
+                value: Colors.amberAccent,
+                groupValue: _defaultColor,
+                onChanged: (value)
+                {
+                  setState(()
+                  {
+                    _onColorSeleted(Colors.amberAccent);
+                  });
+                }),
+
+            RadioListTile(
+                title: Text("Green"),
+                value: Colors.green,
+                groupValue: _defaultColor,
+                onChanged: (value)
+                {
+                  setState(()
+                  {
+                    _onColorSeleted(Colors.green);
+                  });
+                }),
+
+            RadioListTile(
+                title: Text("Blue"),
+                value: Colors.cyan,
+                groupValue: _defaultColor,
+                onChanged: (value){
+                  setState(() {
+                    _onColorSeleted(Colors.cyan);
+                  });
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
